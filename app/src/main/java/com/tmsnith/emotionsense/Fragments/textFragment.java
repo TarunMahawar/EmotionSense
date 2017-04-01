d112a121ab7a98412aa2fe9f5e4d16fcd4cae72b
TarunMahawar <TarunMahawar25@gmail.com>
2017-04-01 13:13:45 +0530

    Fragments added on home page

--- app/src/main/java/com/tmsnith/emotionsense/Fragments/textFragment.java
+++ app/src/main/java/com/tmsnith/emotionsense/Fragments/textFragment.java
@@ -1,11 +1,16 @@
 package com.tmsnith.emotionsense.Fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.tmsnith.emotionsense.R;
 
@@ -14,6 +19,9 @@ import com.tmsnith.emotionsense.R;
  */
 public class textFragment extends Fragment {
 
+
+    EditText text;
+    Button submit;
 
     public textFragment() {
         // Required empty public constructor
@@ -24,8 +32,31 @@ public class textFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        View v =  inflater.inflate(R.layout.fragment_text, container, false);
+        text = (EditText) v.findViewById(R.id.text);
+        submit = (Button) v.findViewById(R.id.submit1);
+        submit.setText("ssss");
+        Log.v("successful1", "sucessful1");
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Successful", Toast.LENGTH_SHORT).show();
+                Log.v("successful", "sucessful");
+            }
+        });
+
+//        return null;
         return inflater.inflate(R.layout.fragment_text, container, false);
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+
 
     public static textFragment get()
     {
