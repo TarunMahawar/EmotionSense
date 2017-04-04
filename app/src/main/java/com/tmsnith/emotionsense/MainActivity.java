93c2a3377b29a4899ad2effaa3bc1ba4f8644e19
TarunMahawar <TarunMahawar25@gmail.com>
2017-04-04 10:42:13 +0530

    Menu added to MainActivity and app icon changed

--- app/src/main/java/com/tmsnith/emotionsense/MainActivity.java
+++ app/src/main/java/com/tmsnith/emotionsense/MainActivity.java
@@ -1,5 +1,8 @@
 package com.tmsnith.emotionsense;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,6 +11,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.tmsnith.emotionsense.Fragments.imageFragment;
 import com.tmsnith.emotionsense.Fragments.textFragment;
@@ -32,22 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
         MyPagerAdapter adapter=new MyPagerAdapter(getSupportFragmentManager());
         pager.setAdapter(adapter);
-
-//        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//
-//            }
-//        });
 
         tab.setupWithViewPager(pager);
 
@@ -95,4 +84,40 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.homepage_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.contact) {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            String uriText = "mailto:" +
+                    Uri.encode("tarunmahawar25@gmail.com")
+                    +"?subject=" + Uri.encode("Feedback / Reporting a Bug") + "&body=" +
+                    Uri.encode("Hello developers, \nI want to report a bug/give feedback corresponding to this app. \n\n.....\n\n-Your name");
+
+            Uri uri = Uri.parse(uriText);
+            intent.setData(uri);
+            startActivity(Intent.createChooser(intent, "Send Email"));
+
+            return true;
+        }else if(id==R.id.about){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(String.format("%1$s", getString(R.string.app_name)));
+            builder.setMessage(getResources().getText(R.string.about_us));
+            builder.setPositiveButton("OK", null);
+            builder.setIcon(R.drawable.app_icon);
+            AlertDialog welcomeAlert = builder.create();
+            welcomeAlert.show();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
